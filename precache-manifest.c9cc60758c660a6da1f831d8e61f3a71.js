a0434b9e6dcd5331898acd25ce8a5bca008461fe
Kate Davis <katedavis@Kates-MacBook-Pro.local>
2020-02-27 21:44:58 -0600

    Updates

---
+++ precache-manifest.c9cc60758c660a6da1f831d8e61f3a71.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3283377e446b989dc46a61174283ed",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "b39decca0bd2d20578c7",
+    "url": "/clickygame/static/css/main.684169fe.chunk.css"
+  },
+  {
+    "revision": "66741b3b0dedd59e8b24",
+    "url": "/clickygame/static/js/2.cb30382c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clickygame/static/js/2.cb30382c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39decca0bd2d20578c7",
+    "url": "/clickygame/static/js/main.28335b94.chunk.js"
+  },
+  {
+    "revision": "6596119a575b8653bd7b",
+    "url": "/clickygame/static/js/runtime-main.9a0c2231.js"
+  },
+  {
+    "revision": "b73e94bf7ba9886bc4f1ffdf9265964a",
+    "url": "/clickygame/static/media/arya.b73e94bf.png"
+  },
+  {
+    "revision": "d7dfed8108fc31316e51385949563a9e",
+    "url": "/clickygame/static/media/bran.d7dfed81.png"
+  },
+  {
+    "revision": "4eeed792716c7d1d4e7e8b5979934cbe",
+    "url": "/clickygame/static/media/brienne.4eeed792.png"
+  },
+  {
+    "revision": "cfb1c50044d5f5dc10839203ead09dd2",
+    "url": "/clickygame/static/media/cersei.cfb1c500.png"
+  },
+  {
+    "revision": "4feb96b30855b7015c3ce97f30af59ca",
+    "url": "/clickygame/static/media/daeny.4feb96b3.png"
+  },
+  {
+    "revision": "3270b2b263dcfd1d818475dd04bfa3bd",
+    "url": "/clickygame/static/media/jaime.3270b2b2.png"
+  },
+  {
+    "revision": "34dcdd8801ba7f31a77dd7fa7d7f1423",
+    "url": "/clickygame/static/media/jon.34dcdd88.png"
+  },
+  {
+    "revision": "b64dca8d2f0927b967df4e383f286d7e",
+    "url": "/clickygame/static/media/sansa.b64dca8d.png"
+  },
+  {
+    "revision": "78e9a69ba66a5f297ac470fd1e9e0921",
+    "url": "/clickygame/static/media/theon.78e9a69b.png"
+  },
+  {
+    "revision": "363a2b253db96391fcc2bd3e4b126b70",
+    "url": "/clickygame/static/media/tormund.363a2b25.png"
+  },
+  {
+    "revision": "73ffe97a7fe5300df43f9484ca97aa51",
+    "url": "/clickygame/static/media/tyrion.73ffe97a.png"
+  }
+]);
